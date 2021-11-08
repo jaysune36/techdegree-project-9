de55f3254efd95d221578258ae347eacef520ff8
user <user@users-MacBook-Pro.local>
2021-11-08 12:13:49 -0800

    add opening slide effect when skills section or when an img is clicked to display info about skills or img clicked. Added new SVG files to replace old ones and added to layout. Created effect when img is clicked to display img in color and back to grayscale when clicked off

--- app.js
+++ app.js
@@ -2,24 +2,71 @@ document.addEventListener('DOMContentLoaded', ()=> {
   const aboutInfo = document.getElementById('about');
   const nav = document.querySelector('.nav-bar');
   const info = aboutInfo.querySelector('.display');
+  const skillsList = document.getElementById('skills')
+  const portfolio = document.getElementById('portfolio');
+
+  function slideDownShow(element) {
+    element.classList.toggle('slide-down');
+    element.style.overflow = '';
+    element.style.maxHeight = '100%';
+  }
+
+  function slideDownHide(element) {
+    element.classList.toggle('slide-down');
+    element.style.overflow = 'hidden';
+    element.style.maxHeight = '0';
+  }
 
   aboutInfo.addEventListener('click', (e) => {
     if(e.target !== aboutInfo) {
       const aboutSection = e.target.closest('section');
       if(aboutSection){
         if(info.className === 'display') {
-        info.classList.toggle('slide-down');
-        info.style.overflow = '';
-        info.style.maxHeight = '100%';
+        slideDownShow(info);
       } else {
-        info.classList.toggle('slide-down');
-        info.style.overflow = 'hidden';
-        info.style.maxHeight = '0';
+        slideDownHide(info);
       }
     }
     } 
   })
 
+  portfolio.addEventListener('click', (e) => {
+    if(e.target.tagName === 'IMG') {
+      const portfolioInfo = e.target.nextElementSibling.firstElementChild;
+      if(portfolioInfo.className === '') {
+      e.target.classList.toggle('no-filter');
+      slideDownShow(portfolioInfo);
+      } else  {
+        e.target.classList.toggle('no-filter');
+        slideDownHide(portfolioInfo)
+      }
+    }
+  })
 
+  skillsList.addEventListener('click', (e) => {
+    if(e.target !== skillsList) {
+      const skillSection = e.target.closest('section');
+      const skillItems = skillsList.querySelectorAll('.skill');
+      if(skillSection) {
+        if(skillSection.className === '') {
+          skillSection.className = 'open'
+        let counter = 0;
+        setInterval(() => {
+          if(counter < 4) {
+          console.log(counter);
+          slideDownShow(skillItems[counter])
+          counter++
+          }
+        }, 40);
+      } else {
+        skillSection.className = '';
+        for(let i=0; i < skillItems.length; i++) {
+          let skillItem = skillItems[i];
+          slideDownHide(skillItem);
+        }
+      }
+    }
+    }
+  })
 
 })
