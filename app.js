96c8cb7eb9ee21dba8b795b186a61d038339665f
user <user@users-MacBook-Pro.local>
2021-12-03 15:22:39 -0800

    working on adding svg images to portfolio items to dispay skill used on each project. Formatted JS file and got rid of unsued variables

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const info = aboutInfo.querySelector('.display');
   const skillsList = document.getElementById('skills')
   const portfolio = document.getElementById('portfolio');
+  const portfolioList = portfolio.querySelector('.portfolio-list');
   let portfolioItems = document.querySelectorAll('.portfolio-item');
   const modal = document.getElementById('modal');
   const contactInfo = document.getElementById('contact');
   const media768 = window.matchMedia('(max-width: 768px)');
-
 
   function slideDownShow(element) {
       element.classList.toggle('slide-down');
@@ -32,9 +32,16 @@ document.addEventListener('DOMContentLoaded', () => {
     <div class='port-modal-info'>
       ${item.lastElementChild.innerHTML}
     </div>
-`;
+  `;
     modalBox.insertAdjacentElement('beforeend', div);
   }
+
+  aboutInfo.addEventListener('fullscreenchange', (e) => {
+
+    if(aboutInfo.fullscreenElement) {
+      console.log('this screen has changed')
+    }
+  })
 
   aboutInfo.addEventListener('click', (e) => {
     if (e.target !== aboutInfo) {
@@ -47,85 +54,6 @@ document.addEventListener('DOMContentLoaded', () => {
           slideDownHide(info);
         }
       }
-      }
-    }
-  })
-
-  portfolio.addEventListener('click', (e) => {
-    if (e.target.tagName === 'DIV' || e.target.parentElement.tagName === 'DIV' || e.target.parentElement.parentElement.tagName === 'DIV') {
-      const view = e.target.closest('div');
-      const viewClass = view.className;
-      const portfolioList = portfolio.querySelector('.portfolio-list');
-      const portfolioItems = portfolioList.querySelectorAll('.portfolio-item');
-      let index = 0;
-      if (view.className === 'list-view') {
-        view.className = viewClass + ' active';
-        const listView = view.nextElementSibling;
-        listView.classList.remove('active');
-        portfolioList.classList.remove('grid')
-        for (let i = 0; i < portfolioItems.length; i++) {
-          let portfolioItem = portfolioItems[i];
-          portfolioItem.classList.add('list');
-          portfolioItem.removeAttribute('data-index');
-        }
-        index = 0;
-      }
-      if (view.className === 'grid-view') {
-        view.className = viewClass + ' active';
-        const listView = view.previousElementSibling;
-        listView.classList.remove('active');
-        for (let i = 0; i < portfolioItems.length; i++) {
-          let portfolioItem = portfolioItems[i];
-          portfolioItem.setAttribute('data-index', index);
-          portfolioItem.classList.remove('list');
-          index++
-        }
-        portfolioList.classList.add('grid');
-        console.log(index)
-      }
-    }
-    if(media768.matches) {
-    if (e.target.tagName === 'IMG') {
-      const portfolioInfo = e.target.nextElementSibling.lastElementChild;
-      if (portfolioInfo.className === '') {
-        e.target.classList.toggle('no-filter');
-        slideDownShow(portfolioInfo);
-      } else {
-        e.target.classList.toggle('no-filter');
-        slideDownHide(portfolioInfo)
-      }
-    }
-  }
-    if (e.target.parentElement.parentElement.className.includes('grid') && !media768.matches) {
-      if (e.target.tagName === 'IMG') {
-        modal.style.display = 'block';
-        let portModalIndex = e.target.parentElement.getAttribute('data-index');
-        createModal(portfolioItems[portModalIndex], portModalIndex);
-      }
-    }
-  })
-
-  modal.addEventListener('click', (e) => {
-    const portfolioList = portfolio.querySelector('.portfolio-list');
-    const portfolioItems = portfolioList.querySelectorAll('.portfolio-item');
-    const portModal = document.querySelector('.port-item-info');
-    let portIndex = portModal.getAttribute('data-index');
-    if (e.target.className === 'modal-close') {
-      modal.style.display = 'none';
-      modal.lastElementChild.firstElementChild.nextElementSibling.remove();
-    }
-    if (e.target.className === 'scroll-left') {
-      if(portIndex > 0) {
-      portModal.remove()
-      createModal(portfolioItems[parseFloat(portIndex) - 1], parseFloat(portIndex) - 1);
-      } else {
-        return null;
-      }
-    }
-    if (e.target.className === 'scroll-right') {
-      if(portIndex < (portfolioItems.length - 1)) {
-      portModal.remove()
-      createModal(portfolioItems[parseFloat(portIndex) + 1], parseFloat(portIndex) + 1);
       }
     }
   })
@@ -157,5 +85,78 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   })
 
+  portfolio.addEventListener('click', (e) => {
+    if (e.target.tagName === 'DIV' || e.target.parentElement.tagName === 'DIV' || e.target.parentElement.parentElement.tagName === 'DIV') {
+      const view = e.target.closest('div');
+      const viewClass = view.className;
+      let index = 0;
+      if (view.className === 'list-view') {
+        view.className = viewClass + ' active';
+        const listView = view.nextElementSibling;
+        listView.classList.remove('active');
+        portfolioList.classList.remove('grid')
+        for (let i = 0; i < portfolioItems.length; i++) {
+          let portfolioItem = portfolioItems[i];
+          portfolioItem.classList.add('list');
+          portfolioItem.removeAttribute('data-index');
+        }
+        index = 0;
+      }
+      if (view.className === 'grid-view') {
+        view.className = viewClass + ' active';
+        const listView = view.previousElementSibling;
+        listView.classList.remove('active');
+        for (let i = 0; i < portfolioItems.length; i++) {
+          let portfolioItem = portfolioItems[i];
+          portfolioItem.setAttribute('data-index', index);
+          portfolioItem.classList.remove('list');
+          index++
+        }
+        portfolioList.classList.add('grid');
+      }
+    }
+    if(media768.matches) {
+    if (e.target.tagName === 'IMG') {
+      const portfolioInfo = e.target.nextElementSibling.lastElementChild;
+      if (portfolioInfo.className === '') {
+        e.target.classList.toggle('no-filter');
+        slideDownShow(portfolioInfo);
+      } else {
+        e.target.classList.toggle('no-filter');
+        slideDownHide(portfolioInfo)
+      }
+    }
+  }
+    if (e.target.parentElement.parentElement.className.includes('grid') && !media768.matches) {
+      if (e.target.tagName === 'IMG') {
+        modal.style.display = 'block';
+        let portModalIndex = e.target.parentElement.getAttribute('data-index');
+        createModal(portfolioItems[portModalIndex], portModalIndex);
+      }
+    }
+  })
+
+  modal.addEventListener('click', (e) => {
+    const portModal = document.querySelector('.port-item-info');
+    let portIndex = portModal.getAttribute('data-index');
+    if (e.target.className === 'modal-close') {
+      modal.style.display = 'none';
+      modal.lastElementChild.firstElementChild.nextElementSibling.remove();
+    }
+    if (e.target.className === 'scroll-left') {
+      if(portIndex > 0) {
+      portModal.remove()
+      createModal(portfolioItems[parseFloat(portIndex) - 1], parseFloat(portIndex) - 1);
+      } else {
+        return null;
+      }
+    }
+    if (e.target.className === 'scroll-right') {
+      if(portIndex < (portfolioItems.length - 1)) {
+      portModal.remove()
+      createModal(portfolioItems[parseFloat(portIndex) + 1], parseFloat(portIndex) + 1);
+      }
+    }
+  })
 
 })
